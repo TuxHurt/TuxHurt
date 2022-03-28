055ab0168503624c53fd0f0eb7b37f0eeab836f3
Woffle <79484501+WoffleTbh@users.noreply.github.com>
2022-03-28 22:22:42 +0200

    Added some stuffs
    Some of my pr's take a while to be merged (if ever), so I added a `-r` flag which simply let's you change which repository to pull scripts from. Another change is simply using `getche` for 1 letter inputs

--- update.py
+++ update.py
@@ -4,13 +4,11 @@ import configparser
 import argparse
 import github
 import datetime
+import getch
 from colorama import Fore, Style
 
 config = configparser.ConfigParser()
 config.read("sirhurt/TuxHurtConfig.ini")
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--force", required=False, help="Will force update even if you're running the latest version", action="store_true")
-args = vars(ap.parse_args())
 
 def updateScript(script):
     print(f"Updating {script}.py...", end="")
@@ -19,6 +17,7 @@ def updateScript(script):
     if output.stderr != b'SSL_INIT\n':
         print(Fore.RED + "There was an error while fetching the " + script + ".py script.\nReverting changes..." + Style.RESET_ALL)
         os.system(f"mv {script}.py.backup {script}.py")
+        os.remove(f"{script}.py.1")
     else:
         os.remove(f"{script}.py.backup")
         os.system(f"mv {script}.py.1 {script}.py")
@@ -27,20 +26,28 @@ def updateScript(script):
 def checkTuxUpdate(manual=False, force=False):
     if not manual:
         os.chdir("..")
+    config.read("sirhurt/TuxHurtConfig.ini")
+    try:
+        repo = config.get("DEFAULT", "repo")
+    except Exception as e:
+        config.set("DEFAULT", "repo", "TuxHurt/TuxHurt")
+        repo = "TuxHurt/TuxHurt"
     print(Fore.YELLOW + "Checking for TuxHurt updates..." + Style.RESET_ALL)
     g = github.Github()
-    repo = g.get_repo("TuxHurt/TuxHurt")
+    repo = g.get_repo(repo)
     try:
         last_update = datetime.datetime.strptime(config.get("DEFAULT", "lastupdate")[2:], '%y-%m-%d %H:%M:%S')
     except Exception as e:
         last_update = repo.pushed_at
     config.set("DEFAULT", "lastupdate", str(repo.pushed_at))
     update = False
+    last_update = datetime.datetime(20, 10, 10)
     if not force and repo.pushed_at > last_update:
-        update = input(Fore.GREEN + "A new version of TuxHurt has been detected, would you like to update? [Y/n]: " + Style.RESET_ALL)
-        if update in "no" and update != "":
-            print(Fore.YELLOW + "Skipping update..." + Style.RESET_ALL)
-        update = update == "" or update not in "no"
+        print(Fore.GREEN + "A new version of TuxHurt has been detected, would you like to update? [Y/n]: " + Style.RESET_ALL, end="", flush=True)
+        update = getch.getche().lower()
+        if update == "n":
+            print(Fore.YELLOW + "\nSkipping update..." + Style.RESET_ALL)
+        update = update == "" or update != "n"
     elif repo.pushed_at == last_update:
         print(Fore.GREEN + "You're running the latest version!" + Style.RESET_ALL)
         if force:
@@ -51,10 +58,10 @@ def checkTuxUpdate(manual=False, force=False):
         updateScript("run")
         updateScript("setup")
         updateScript("update")
-    with open("sirhurt/TuxHurtConfig.ini", "w") as configfile:
+    with open("sirhurt/TuxHurtConfig.ini", "w+") as configfile:
         config.write(configfile)
     if not manual:
         os.chdir("sirhurt")
 
 if __name__ == "__main__":
-    checkTuxUpdate(True, args["force"])
+    checkTuxUpdate(True)
